294437226607c3fb81c182f1428dcc7f26161de2
sodiq <sa992@student.le.ac.uk>
2024-11-01 10:07:12 +0000

    Firebase Configuration Set Up

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCB-V87wSWz5P_Wnz2O6qQ7n_u8GJ0EGoQ",
+  authDomain: "pennyup-aade4.firebaseapp.com",
+  projectId: "pennyup-aade4",
+  storageBucket: "pennyup-aade4.firebasestorage.app",
+  messagingSenderId: "329368523535",
+  appId: "1:329368523535:web:1f1c0a096d90d62658647a",
+  measurementId: "G-2X917YC9PS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
